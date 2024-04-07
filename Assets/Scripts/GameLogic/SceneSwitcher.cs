bf78ba5cf0a14de5a0073ff5158f5e2117f8f4cb
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-04-07 15:30:03 +0300

    ф

---
+++ Assets/Scripts/GameLogic/SceneSwitcher.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace Assets.Scripts.GameLogic
+{
+    internal class SceneSwitcher : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }
+    }
+}
