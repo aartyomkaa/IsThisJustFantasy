e716cf5a1b890d4f5f93394df31c8ac1ce402e8f
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-04-30 18:53:28 +0300

    Tutorial

---
+++ Assets/Scripts/UI/TutorialArrow.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts.PlayerComponents;
+using UnityEngine;
+
+namespace Assets.Scripts.UI
+{
+    internal class TutorialArrow : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out Player player))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
