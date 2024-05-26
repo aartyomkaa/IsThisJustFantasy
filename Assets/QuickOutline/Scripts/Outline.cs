66c959fbfc0a463cc049611155bbbfd71a751a96
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-05-27 01:52:43 +0300

    37

--- Assets/QuickOutline/Scripts/Outline.cs
+++ Assets/QuickOutline/Scripts/Outline.cs
@@ -188,8 +188,7 @@ public class Outline : MonoBehaviour {
       }
 
       // Retrieve or generate smooth normals
-      var index = bakeKeys.IndexOf(meshFilter.sharedMesh);
-      var smoothNormals = (index >= 0) ? bakeValues[index].data : SmoothNormals(meshFilter.sharedMesh);
+      var smoothNormals = SmoothNormals(meshFilter.sharedMesh);
 
       // Store smooth normals in UV3
       meshFilter.sharedMesh.SetUVs(3, smoothNormals);
