2482119cd3f4d252cd6e33ea77995bfdf07a454a
aartyomkaa <114865637+aartyomkaa@users.noreply.github.com>
2024-06-12 23:03:05 +0300

    timer

--- Assets/Scripts/YandexSDK/InterstitialAdShower.cs
+++ Assets/Scripts/YandexSDK/InterstitialAdShower.cs
@@ -1,12 +1,44 @@
 ﻿using Agava.YandexGames;
+using System;
+using System.Collections;
+using UnityEngine;
 
 namespace Assets.Scripts.YandexSDK
 {
     internal class InterstitialAdShower : AdShower
     {
+        private float _cooldownTime = 60.5f;
+        private float _timeLeft;
+        private bool _isOnCooldown;
+
+        public event Action<float> CooldownTimeLeft;
+
         public override void Show()
         {
-            InterstitialAd.Show(OnOpenCallBack, OnCloseCallBack);
+            if (_isOnCooldown)
+            {
+                CooldownTimeLeft?.Invoke(_timeLeft);
+            }
+            else
+            {
+                StartCoroutine(Timer());
+                InterstitialAd.Show(OnOpenCallBack, OnCloseCallBack);
+            }
+        }
+
+        private IEnumerator Timer()
+        {
+            _isOnCooldown = true;
+            _timeLeft = _cooldownTime;
+
+            while (_timeLeft > 0)
+            {
+                _timeLeft -= Time.fixedDeltaTime;
+
+                yield return Time.fixedDeltaTime;
+            }
+
+            _isOnCooldown = false;
         }
     }
 }
